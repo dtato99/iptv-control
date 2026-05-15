670373420a946d241b2e4db13efa423c9fecbd73
dtato99 <tatod1999@gmail.com>
2026-05-15 13:32:48 -0300

    Change redirect to 404 for non-Sparkle users

--- api/lista.js
+++ api/lista.js
@@ -1,10 +1,8 @@
 export default async function handler(req, res) {
 
-  // LISTA REAL
   const REAL_URL =
     "https://drive.google.com/uc?export=download&id=18DyvSvMy5_5E12gCjWlr3oHrEAMoFiSv";
 
-  // USER AGENT
   const ua = (req.headers["user-agent"] || "").toLowerCase();
 
   // SOLO SPARKLE TV
@@ -13,33 +11,29 @@ export default async function handler(req, res) {
     ua.includes("sparkletv") ||
     ua.includes("sparkle tv");
 
-  // TODO LO DEMÁS -> GOOGLE
+  // TODO LO DEMÁS -> ERROR SILENCIOSO
   if (!esSparkle) {
 
-    return res.redirect("https://google.com");
+    return res.status(404).send("Not found");
 
   }
 
   try {
 
-    // DESCARGAR LISTA
     const response = await fetch(REAL_URL);
 
     const text = await response.text();
 
-    // FORMATO IPTV
     res.setHeader(
       "Content-Type",
       "audio/x-mpegurl"
     );
 
-    // EVITAR CACHE
     res.setHeader(
       "Cache-Control",
       "no-store"
     );
 
-    // ENVIAR LISTA
     return res.status(200).send(text);
 
   } catch (e) {
