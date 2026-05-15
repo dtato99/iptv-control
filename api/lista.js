0fbc7a41e78a4383f0e7b4c75aad4744a554deae
dtato99 <tatod1999@gmail.com>
2026-05-15 00:02:05 -0300

    Enhance lista.js with comments and structure
    Added comments for clarity and improved code structure.

--- api/lista.js
+++ api/lista.js
@@ -1,9 +1,10 @@
 export default async function handler(req, res) {
 
+  // LISTA REAL
   const REAL_URL =
-    "const REAL_URL =
-  "https://drive.google.com/uc?export=download&id=18DyvSvMy5_5E12gCjWlr3oHrEAMoFiSv";
+    "https://drive.google.com/uc?export=download&id=18DyvSvMy5_5E12gCjWlr3oHrEAMoFiSv";
 
+  // USER AGENT
   const ua = (req.headers["user-agent"] || "").toLowerCase();
 
   // SOLO SPARKLE TV
@@ -14,25 +15,31 @@ export default async function handler(req, res) {
 
   // TODO LO DEMÁS -> GOOGLE
   if (!esSparkle) {
+
     return res.redirect("https://google.com");
+
   }
 
   try {
 
+    // DESCARGAR LISTA
     const response = await fetch(REAL_URL);
 
     const text = await response.text();
 
+    // FORMATO IPTV
     res.setHeader(
       "Content-Type",
       "audio/x-mpegurl"
     );
 
+    // EVITAR CACHE
     res.setHeader(
       "Cache-Control",
       "no-store"
     );
 
+    // ENVIAR LISTA
     return res.status(200).send(text);
 
   } catch (e) {
